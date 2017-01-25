2188ff1a0de1ff5099b9aa235c19ca0a838b7aae
Elgrogz <gregorgilchrist@gmail.com>
2017-01-25 12:07:54 +0000

    can get country capital and population info

--- public/index.js
+++ public/index.js
@@ -13,27 +13,45 @@ var populateList = function(countries) {
     countries.forEach(function(country) {
       var option = document.createElement('option');
       option.innerText = country.name;
+      option.value = country.name;
       select.appendChild(option);
     });
+  }
+
+  var getInfo = function() {
+    var container = document.querySelector('#country-info');
+    data.forEach(function(country){
+      if (country.name === this.value){
+        console.log(country.name);
+        var ul = document.createElement('ul');
+        ul.innerText = country.name;
+          var population = document.createElement('li');
+          population.innerText = country.population;
+          ul.appendChild(population);
+          var capital = document.createElement('li');
+          capital.innerText = country.capital;
+          ul.appendChild(capital);
+          container.appendChild(ul);
+                
+      }
+    }.bind(this));
   }
 
   var requestComplete = function() {
     if (this.status !== 200) return;
     var jsonString = this.responseText;
-    var data = JSON.parse(jsonString);
-
+    data = JSON.parse(jsonString);
 
     populateList(data);
   }
 
   var app = function() {
-    var listOfcountries = document.querySelector('#list-of-countries');
-    var url = "https://restcountries.eu/rest/v1/all";
+      var url = "https://restcountries.eu/rest/v1/all";
       makeRequest(url, requestComplete);  
+
+      var listOfcountries = document.querySelector('#list-of-countries');
     
-    listOfcountries.onchange = function() {
-      
-    }
+    listOfcountries.onchange = getInfo;
 
   }
 
